f09cede94e4a239af914221a5fd4d4e587c99766
Taylor <taylous99@gmail.com>
2019-12-08 19:03:31 +0900

    Processing new project env
    ## Content
     Created a new project to use the unit test (Jest).

---
+++ src/components/base/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <v-app-bar
+            color="blue lighten-3 accent-2"
+            dark
+        >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Page title</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+            <v-menu
+                left
+                bottom
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
+
+                    <v-list>
+                    <v-list-item
+                        v-for="url in address"
+                        :key="url"
+                    >
+                        <router-link :to="'/' + url">
+                            <v-list-item-title>{{ url }}</v-list-item-title>
+                        </router-link>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            address: [
+                "home",
+                "login",
+                "register"
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+
+    a:link, a:visited {
+
+        color: black;
+        text-decoration: none;
+    }
+</style>
---
+++ src/components/user/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-card
+        class="mx-auto"
+        max-width="90%"
+    >
+        <v-img
+            class="mx-auto"
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        ></v-img>
+
+        <v-card-title>
+            Login Page
+        </v-card-title>
+
+        <v-card-subtitle>
+            Toy Project - Vue.js
+        </v-card-subtitle>
+
+        <v-text-field
+            v-model="email"
+            label="E-MAIL"
+            outlined
+            clearable
+        ></v-text-field>
+
+        <v-text-field
+            v-model="password"
+            type="password"
+            label="PASSWORD"
+            outlined
+            clearable
+        ></v-text-field>
+
+        <v-card-actions>
+            <v-btn
+                color="green"
+                text
+            >
+                LOGIN
+            </v-btn>
+            <v-spacer></v-spacer>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            email: "",
+            password: ""
+        };
+    }
+};
+</script>
