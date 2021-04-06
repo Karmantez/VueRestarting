ea96a0ff86e73ab3a8ccd4b68800e50ad2878f41
taylous <taylous99@gmail.com>
2021-04-06 14:49:49 +0900

    implement note component
    note component를 만들고 back-end로부터 데이터를 받아오는 로직까지 완성했습니다.

--- vue-diary-frontend/src/views/MainPage.vue
+++ vue-diary-frontend/src/views/MainPage.vue
@@ -1,9 +1,46 @@
 <template>
-  <div><h1>MAIN</h1></div>
+  <div>
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import PostListItem from '@/components/posts/PostListItem';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+import { fetchPosts } from '@/api';
 
-<style></style>
+export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      this.postItems = data.posts;
+    },
+  },
+};
+</script>
