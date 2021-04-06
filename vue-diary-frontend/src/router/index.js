f2038ec0c542dbea6bda997965416499d2854608
taylous <taylous99@gmail.com>
2021-04-06 16:34:52 +0900

    implement add post function
    post 작성 기능을 구현하였습니다.
    아직 삭제/수정 기능은 미구현 상태입니다.

--- vue-diary-frontend/src/router/index.js
+++ vue-diary-frontend/src/router/index.js
@@ -21,6 +21,10 @@ const routes = [
     component: () => import('@/views/MainPage.vue'),
   },
   {
+    path: '/add',
+    component: () => import('@/views/PostAddPage.vue'),
+  },
+  {
     path: '*',
     component: () => import('@/views/NotFoundPage.vue'),
   },
